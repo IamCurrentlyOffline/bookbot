53f71aa6fde99295d21ea3b21fe4864a0ddce4e8
IamCurrentlyOffline <OfflineSoftwareandDesign@gmail.com>
2025-04-10 09:03:01 -0500

    added and printed character counter for books

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, count_chars
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,6 +9,11 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(book_text)
     print(f"{num_words} words found in the document")
+
+    char_counts = count_chars(book_text)
+
+    for char, count in char_counts.items():
+        print(f"'{char}': {count}")
 
 main()
 

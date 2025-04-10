b0038f00fce3456038610c5df24170273be1e3d3
IamCurrentlyOffline <OfflineSoftwareandDesign@gmail.com>
2025-04-10 08:38:44 -0500

    Added character counter

--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def count_chars(text):
+    counter = {}
+    for char in text:
+        char = char.lower()
+        if char in counter:
+            counter[char] += 1
+        else:
+            counter[char] = 1
+    
+    return counter

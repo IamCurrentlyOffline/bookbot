d42644d4b8055df316e0349a23eb7bb8c0010faf
IamCurrentlyOffline <OfflineSoftwareandDesign@gmail.com>
2025-04-09 20:56:10 -0500

    Added word counter + print number of words

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ def get_book_text(filepath):
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
-    
+    num_words = count_words(book_text)
+    print(f"{num_words} words found in the document")
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
 main()
 
 # define multiple things to get proper readabiltiy and function

80bf7dd17c9b022ab113265dfa83e1ee9771e826
IamCurrentlyOffline <OfflineSoftwareandDesign@gmail.com>
2025-04-10 13:44:55 -0500

    Reports / Arguments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, count_chars, sort_char_count
 
 
@@ -8,10 +9,13 @@ def get_book_text(filepath):
 
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
     book_text = get_book_text(filepath)
 
-    # Get word count
     num_words = get_num_words(book_text)
 
     # Get character counts and sort them
